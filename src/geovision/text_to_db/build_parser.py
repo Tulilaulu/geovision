1120bb7e7546391cbc43cda4cd991753960bd117
Tuomas Tynkkynen <dezgeg@gmail.com>
2011-06-17 14:18:31 +0300

    Removed useless foreign keys from models & updated parsers to match.

---
+++ src/geovision/text_to_db/build_parser.py
@@ -0,0 +1,32 @@
+from geovision.viz.models import Result, Read, DbEntry
+from geovision.text_to_db.bulk_inserter import BulkInserter
+
+import os
+
+DEBUG = False
+
+def parseBuilds(sample_name, db_name, filehandle):
+	"""Parse build data from a .build file and store it to the db.
+		sample_name: name of the sample (eg. OLKR49)
+		db_name: name of the database (eg. frnadb)
+		filehandle: file handle of the data file
+	"""
+	inserter = BulkInserter(Result)
+	for line in filehandle:
+		(read_id, db_seq_id, evident_type, ec_number, error_value, bitscore) = line.strip().split("\t")
+#		db_entry = DbEntry.objects.get(db_id__startswith=('sp|' + db_seq_id), source_file=db_name)
+#		read = Read.objects.get(read_id=read_id, sample=sample_name)
+		if DEBUG:
+			from django.db import connection
+			for q in connection.queries:
+				print q['sql']
+		inserter.save(Result(read=read_id, db_entry=db_seq_id, evident_type=evident_type, ec_number=ec_number,
+			error_value=error_value, bitscore=bitscore))
+	inserter.close()
+
+if __name__ == '__main__':
+	import sys
+	filename = sys.argv[1]
+	(sample_name, db_name, extension) = os.path.basename(filename).split('.')
+	parseBuilds(sample_name, db_name, open(filename, 'r'))
+	
---
+++ src/geovision/viz/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from geovision.userdb.models import Sample
+
+class Read(models.Model):
+	sample = models.CharField(max_length=32)
+	read_id = models.CharField(max_length=64)
+	description = models.TextField()
+	data = models.TextField()
+	class Meta:
+		unique_together = ("sample", "read_id")
+
+class DbEntry(models.Model):
+	source_file = models.CharField(max_length=32)
+	db_id = models.CharField(max_length=32, primary_key=True)
+	description = models.TextField()
+	data = models.TextField()
+	# Uniprot-specific fields below
+	sub_db = models.CharField(max_length=4, blank=True)
+	entry_name = models.CharField(max_length=16, blank=True)
+	os_field = models.CharField(max_length=128, blank=True)
+	other_info = models.TextField(blank=True)
+
+class DbUniprotEcs(models.Model):
+	db_id = models.CharField(max_length=32)
+	protein_existence_type = models.CharField(max_length=4)
+	ecs = models.CharField(max_length=128)
+
+class Blast(models.Model): # qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore
+	read = models.CharField(max_length=64)
+	database_name = models.CharField(max_length=16)
+	db_entry = models.CharField(max_length=32)
+	pident = models.DecimalField(max_digits=4,decimal_places=2)
+	length = models.IntegerField()
+	mismatch = models.IntegerField()
+	gapopen = models.IntegerField()
+	qstart = models.IntegerField()
+	qend = models.IntegerField()
+	sstart = models.IntegerField()
+	send = models.IntegerField()
+	error_value = models.FloatField()
+	bitscore = models.FloatField()
+
+class Result(models.Model): # Query_seq_id    Target_seq_id   Evident_type    E.C._number     p_value Bit_score
+	read = models.CharField(max_length=64)
+	db_entry = models.CharField(max_length=32)
+	evident_type = models.CharField(max_length = 2)
+	ec_number = models.CharField(max_length = 32)
+	error_value = models.FloatField()
+	bitscore = models.FloatField()
