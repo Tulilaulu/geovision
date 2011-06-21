4b4e78ed92524b48145cec8f684ca63ef39c264f
Lasse Tyrv?inen <sundokan@gmail.com>
2011-06-22 00:41:10 +0300

    Test json file with edge coloring

---
+++ src/geovision/static/json_file.js
@@ -0,0 +1,76 @@
+var json_data = [{
+                "id":"1.1.1.1",
+                "name":"1.1.1.1",
+				"data": { node_type: "ec", description: "<b>1.1.1.1:</b></br></br></br>", adjacencies: "<b>Adjacent nodes: </b></br>DB entries: </br>'P73138'</br>'A1A835'</br>'Q1RFI7'</br>'Q0TKS7'</br>"},
+                "adjacencies":[
+					{
+                        "nodeTo":"P73138",
+                        "data": {
+							"$color": "#f0f"
+                        }
+                },{
+                        "nodeTo": "03",
+                        "data": {
+
+                        }
+                },{
+                        "nodeTo": "04",
+                        "data": {
+
+                        }
+                },{
+                        "nodeTo": "05",
+                        "data": {
+
+                        }
+                }]
+        },{
+                "id":"P73138",
+                "name":"P73138",
+                "adjacencies":[]
+        },{
+                "id":"03",
+                "name":"03",
+                "adjacencies":[{
+                        "nodeTo":"04",
+                        "data": {
+
+                        }
+                },{
+                        "nodeTo": "1.1.1.1",
+                        "data": {
+
+                        }
+                }]
+        },{
+                "id":"04",
+                "name":"04",
+                "adjacencies":[{
+                        "nodeTo":"05",
+                        "data": {
+							"$color": "#0f0"
+                        }
+                }]
+        },{
+                "id":"05",
+                "name":"05"
+        },{
+                "id":"06",
+                "name":"06",
+                "adjacencies":[{
+                        "nodeTo":"1.1.1.1",
+                        "data": {
+
+                        }
+                }]
+        },{
+                "id":"07",
+                "name":"07",
+                "adjacencies":[{
+                        "nodeTo":"1.1.1.1",
+                        "data": {
+
+                        }
+                }]
+        }
+        ];
