f334f4069ef26e4559dc2d3840bad9bf4b9405a3
Lasse Tyrv?inen <sundokan@gmail.com>
2011-06-29 13:01:38 +0300

    Moved graph_JSON tests to text_to_db.tests package

---
+++ src/geovision/text_to_db/tests/sample_parser_tests.py
@@ -0,0 +1,40 @@
+#coding: UTF-8
+
+import unittest
+import sample_parser
+from geovision.settings import TEST_FILE_PATH
+
+class SampleParserTests(unittest.TestCase):
+	#def setUp(self):
+    #    self.foo = Test_sample_parser()
+    #
+
+    #def tearDown(self):
+    #    self.foo.dispose()
+    #    self.foo = None
+
+	def test_sample_parse_first(self):
+		parser = sample_parser.SamplefileParser(TEST_FILE_PATH + "sample_test.txt")
+		read = parser.next_read()
+		self.assertEqual(read.read_id, 'ensimmainen')
+		self.assertEqual(read.description, 'seliseli')
+		self.assertEqual(read.data, 'ASDFASEGAASGEASGASG')
+
+	def test_sample_parse_last(self):
+		parser = sample_parser.SamplefileParser(TEST_FILE_PATH + "sample_test.txt")
+		parser.next_read()
+		read = parser.next_read()
+		self.assertEqual(read.read_id, 'toinen')
+		self.assertEqual(read.description, 'sulisuli')
+		self.assertEqual(read.data, 'ASGEAGSGASEGAG')
+
+	def test_sample_parse_past_the_end(self):
+		parser = sample_parser.SamplefileParser(TEST_FILE_PATH + "sample_test.txt")
+		parser.next_read()
+		parser.next_read()
+		read = parser.next_read()
+		self.assertEqual(read, None)
+
+if __name__ == '__main__':
+	unittest.main()
+
---
+++ src/geovision/text_to_db/tests/test_graph_JSON.py
@@ -0,0 +1,59 @@
+#To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+import unittest
+from geovision.text_to_db.graph_JSON import *
+import json
+
+def create_blast(**kwargs):
+	kwargs['read'] = Read.objects.get(pk=kwargs['read'])
+	kwargs['db_entry'] = DbEntry.objects.get(pk=kwargs['db_entry'])
+	Blast.objects.create(**kwargs)
+
+class Test_graph_JSONTestCase(unittest.TestCase):
+
+	def setUp(self):
+		Read.objects.all().delete()
+		Read.objects.create(sample="SMPL1", read_id="R1", description="baz", data='ASD')
+		Read.objects.create(sample="SMPL2", read_id="R2", description="baz", data='ASD')
+		Read.objects.create(sample="SMPL3", read_id="R3", description="baz", data='ASD')
+		Read.objects.create(sample="SMPL4", read_id="R4", description="baz", data='ASD')
+		Read.objects.create(sample="SMPL3", read_id="R5", description="baz", data='ASD')
+		Read.objects.create(sample="SMPL2", read_id="R6", description="baz", data='ASD')
+
+		DbEntry.objects.all().delete()
+		DbEntry.objects.create(source_file = "uniprot", db_id = "DB1", description="quux", data='ASD')
+		DbEntry.objects.create(source_file = "uniprot", db_id = "DB2", description="quux", data='ASD')
+		DbEntry.objects.create(source_file = "uniprot", db_id = "DB3", description="quux", data='ASD')
+		DbEntry.objects.create(source_file = "uniprot", db_id = "DB4", description="quux", data='ASD')
+		DbEntry.objects.create(source_file = "uniprot", db_id = "DB5", description="quux", data='ASD')
+		DbEntry.objects.create(source_file = "uniprot", db_id = "DB6", description="quux", data='ASD')
+		DbEntry.objects.create(source_file = "uniprot", db_id = "DB7", description="quux", data='ASD')
+
+		create_blast(read="R1", db_entry="DB1", error_value=0.005, bitscore=200, pident=3, length = 20, mismatch=2, gapopen = 2, qstart= 2, qend = 2, sstart= 3,send=2)
+		create_blast(read="R1", db_entry="DB2", error_value=0.005, bitscore=400, pident=3, length = 20, mismatch=2, gapopen = 2, qstart= 2, qend = 2, sstart= 3,send=2)
+		create_blast(read="R1", db_entry="DB3", error_value=0.005, bitscore=600, pident=3, length = 20, mismatch=2, gapopen = 2, qstart= 2, qend = 2, sstart= 3,send=2)
+		create_blast(read="R1", db_entry="DB4", error_value=0.005, bitscore=800, pident=3, length = 20, mismatch=2, gapopen = 2, qstart= 2, qend = 2, sstart= 3,send=2)
+		create_blast(read="R2", db_entry="DB1", error_value=0.005, bitscore=1000, pident=3, length = 20, mismatch=2, gapopen = 2, qstart= 2, qend = 2, sstart= 3,send=2)
+		create_blast(read="R3", db_entry="DB1", error_value=0.005, bitscore=200, pident=3, length = 20, mismatch=2, gapopen = 2, qstart= 2, qend = 2, sstart= 3,send=2)
+		create_blast(read="R4", db_entry="DB2", error_value=0.005, bitscore=400, pident=3, length = 20, mismatch=2, gapopen = 2, qstart= 2, qend = 2, sstart= 3,send=2)
+		create_blast(read="R5", db_entry="DB3", error_value=0.005, bitscore=700, pident=3, length = 20, mismatch=2, gapopen = 2, qstart= 2, qend = 2, sstart= 3,send=2)
+		create_blast(read="R1", db_entry="DB4", error_value=0.005, bitscore=1100, pident=3, length = 20, mismatch=2, gapopen = 2, qstart= 2, qend = 2, sstart= 3,send=2)
+		create_blast(read="R1", db_entry="DB5", error_value = 0.005, bitscore=1500, pident=3, length = 20, mismatch=2, gapopen = 2, qstart= 2, qend = 2, sstart= 3,send=2)
+
+	def test_node_id(self):
+		a = NodeId("12345", "test")
+		b = NodeId("12345", "test")
+		c = NodeId("asdf", "test")
+		self.assertEqual(a.id, "12345")
+		self.assertEqual(b.type, "test")
+		self.assertEqual(a, b)
+		self.assertNotEqual(a, c)
+
+	def test_query_to_json_init_1(self):
+		q = QueryToJSON(db_entry="DB1", )
+
+
+if __name__ == '__main__':
+    unittest.main()
+
