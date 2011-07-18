84a6761873047c3aeebddf6fd02d5cb1cd6c6519
Tuomas Tynkkynen <dezgeg@gmail.com>
2011-07-18 12:38:19 +0300

    Merged.

---
+++ src/geovision/urls.py
@@ -0,0 +1,27 @@
+from django.conf.urls.defaults import patterns, include, url
+from django.contrib import admin
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+	(r'^$', 'userdb.views.loginpage'),
+	(r'^login$', 'userdb.views.loginpage'),
+	(r'^register$', 'userdb.views.register'),
+	(r'^registering$', 'userdb.views.registering'),
+	(r'^logging_in$', 'userdb.views.logging_in'),
+	(r'^logging_out$', 'userdb.views.logging_out'),
+	(r'^about$', 'userdb.views.about'),
+
+	(r'^testgraph$', 'viz.views.testgraph'),
+	(r'^graphrefresh$', 'viz.views.graphrefresh'),
+	(r'^graphjson$', 'viz.views.graphjson'),
+	(r'^show_alignment$', 'viz.views.show_alignment'),
+	(r'^autocomplete$', 'viz.views.enzyme_autocompletion'),
+	(r'^enzyme_names$', 'viz.views.enzyme_names'),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+	(r'^admin/?', include(admin.site.urls)),
+
+	(r'', 'userdb.views.loginpage'), #everything else regirects to login for now
+)
