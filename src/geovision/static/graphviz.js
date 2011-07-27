2c3096ddf47bad8f8563377c4321fcbc42760bca
Lasse Tyrv?inen <sundokan@gmail.com>
2011-07-27 14:22:58 +0300

    Changed animation function calls to use user animation options

---
+++ src/geovision/static/graphviz.js
@@ -0,0 +1,46 @@
+function prepareJSON(json)
+{
+	for (i in json)
+	{
+		var data = json[i].data;
+		if(data.type == "enzyme")
+			data.$color = '#0000FF';
+		else if (data.type == "dbentry")
+			data.$color = '#00FF00';
+		for(adj in data.adjacencies)
+				data.adjacencies[adj].data.$direction = data.adjacenction[adj].nodeTo;
+	}
+	return json;
+}
+
+
+function initGraph(json)
+{
+	jQuery('#loader').fadeOut();
+	if(!json || json.error_message)
+	{
+		if(json)
+			$("#error").text(json.error_message);
+		openSearch();
+		return;
+	}
+	initContextMenu();
+	$('#infovis').disableSelection();
+
+	rgraph = new RGraph(Config);
+	rgraph.loadJSON(prepareJSON(json), 0);
+
+
+	colorEdges();
+	rgraph.refresh();
+	rgraph.op.options = $jit.util.merge(rgraph.op.options, (settings.animationSettings || {}));
+
+    rgraph.op.contractForTraversal = contractForTraversal;
+	rgraph.op.filterContract = filterContract;
+	rgraph.op.tagParents = tagParents;
+	rgraph.op.tagSubgraph = tagSubgraph;
+	rgraph.op.tagSubnodes = tagSubnodes;
+	rgraph.centerToNode = centerToNode;
+}
+
+
