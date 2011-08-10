5c79461e5c1ae2dbd3ad80260c7a57d6cd866550
Janne Laukkanen <janne.laukkanen83@gmail.com>
2011-08-10 11:13:32 +0300

    canvas changes

---
+++ src/geovision/static/js/events.js
@@ -0,0 +1,59 @@
+Config.Events = 
+{
+	enableForEdges: true,
+	enable : true,
+	type : 'Native', //edge event doesn't work with 'HTML'..
+
+	onClick: function(node, opt)
+	{
+		if(!node || node.nodeFrom)
+			return;
+		if(currentEdge != undefined){
+			
+			rgraph.config.Events.onMouseLeave(currentEdge);
+		}
+		if(currentNode != undefined){
+			
+			rgraph.config.Events.onMouseLeave(currentNode);
+		}
+
+		saveUndoState();
+		fetchJSON(node, true);
+
+		var changed_max = false;
+
+		if(node._depth >= max_level){
+			max_level = node._depth + 2;
+			changed_max = true;
+		}
+
+		if(changed_max){
+			$jit.Graph.Util.computeLevels(rgraph.graph, rgraph.root, 0);
+			rgraph.canvas.canvases[1].opt.numberOfCircles = max_level;
+			rgraph.canvas.canvases[1].plot();
+			changed_max = false;
+		}
+		if(node.data.type == 'enzyme'){
+			$.getJSON('/enzyme_data', { id: node.id }, showEnzymeData);
+		}
+	},
+	onMouseEnter: function(node, eventInfo, e)
+	{
+		if(ctxMenuOpen || busy)
+			return;
+
+		rgraph.canvas.getElement().style.cursor = 'pointer';
+
+		if (node.nodeTo)
+			currentEdge = node;
+		else
+			currentNode = node;
+	},
+	onMouseLeave: function(object, eventInfo, e)
+	{
+		if(ctxMenuOpen || !object || busy)
+			return;
+		currentNode = currentEdge = undefined;
+		rgraph.canvas.getElement().style.cursor = '';
+	}
+};
