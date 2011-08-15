a89627ddb3561d20abd9891b2ed0f7ffcdff9b53
Tuomas Tynkkynen <dezgeg@gmail.com>
2011-08-15 17:39:39 +0300

    Redone import scripts.

---
+++ src/geovision/static/js/moreinfo.js
@@ -0,0 +1,72 @@
+/*Function for showing the alignment of the read and the db-entry*/
+function alignmentfunction(thisid, nodeFrom, nodeTo) {
+	$.getJSON('/show_alignment', {id: thisid}, function (data) { /*get the json with the data*/
+		if(data == null){
+			return false;
+		}
+		alignment = $('<div class="alignment"></div>');
+		var close = $('<div id = "closealign">Close</div>');
+		close.appendTo(alignment);
+		$('<p>Alignment for edge: <b>' + nodeFrom + ' <-> ' + nodeTo + '</b></p>').appendTo(alignment);
+		for ( i = 0; i < data.readseq.length; i++){
+			var alignmentclass = "";
+			if (data.readseq.charAt(i) === data.dbseq.charAt(i)){
+				alignmentclass = " alignsimilarity";
+			}
+			else {
+				alignmentclass="";
+			}
+			var a = $('<span class="alignmentpart'+ alignmentclass +'">' + data.readseq.charAt(i) +'\n'+ data.dbseq.charAt(i)+ '</span>');
+			a.appendTo(alignment);
+		}
+		$('#alignment').after(alignment);
+		alignment.slideDown(300, function() {
+							alignment.css('margin-bottom', '10px');
+		});
+		return true;
+	});
+	return false;
+}
+/*when the close button appears, it's se to work*/
+$('#closealign').live('click', function() {
+	closealignment(this);
+});
+/*Function to close the div-element showing the alignment*/
+function closealignment (button) {
+		button = $(button);
+		var alignment = button.parent();
+		$(alignment).find('*').remove();/*!Hide all elements*/
+		$(button).remove();
+		$(alignment).css('margin-bottom', '0px');
+		$(alignment).slideUp();
+}
+
+/* Function to list all names, reactions and pathways related to an enzyme in the right container */
+function showEnzymeData (json){
+	enzymes = {};
+	rgraph.graph.eachNode(function(n) {
+		if(n.data.type == 'enzyme')
+			enzymes[n.id] = n;
+	});
+	ec = json.id;
+
+	var html = '<br/>';
+	// KEGG pathway url coloring params: pwnumber / ecnumber <TAB> #bgcol,#fgcol / ecnumber <TAB> #bgcol,fgcol ....
+	if(json.pathways)
+	{
+		html += '<strong>Pathways of ' + ec + '</strong><br/>';
+		html += $.map(json.pathways, function(pw){
+			var pathwayEnzymes = $.grep(pw.enzymes, function(x) { return enzymes[x]; });
+			var colorUrl = escape($.map(pathwayEnzymes, function(ec) { return ec + "\t" + enzymes[ec].data.color + ',#000000'; }).join('/'));
+			return  '<a target="_blank" href="http://www.genome.jp/kegg-bin/show_pathway?map' + pw.id + '/' + colorUrl + '">' + pw.id + ': ' + pw.name + '</a><br/>'; }).join('');
+	}
+
+	names = json.names;
+
+	html += '<strong>Other names of ' + ec + ':</strong><br/>';
+	for (name in names){
+		html = html + names[name] + '<br/>';
+	}
+	$('#names').html(html); /*#names is replaced fully when this is called for a new node*/
+	return;
+ }
---
+++ src/geovision/userdb/views.py
@@ -0,0 +1,134 @@
+from django.http import HttpResponse
+from django.template import Context, loader
+from django.shortcuts import render_to_response, redirect
+from django.template import RequestContext
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from django.core.context_processors import csrf
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+import json
+from userdb.models import SavedView
+
+#redirects authenticated users straight in and shows the login form for the rest
+def loginpage(request):
+	if request.user.is_authenticated():
+		return redirect('/graphrefresh')
+	else:
+		return render_to_response("login.html", { }, context_instance=RequestContext(request) )
+#shows the page with the registering form
+def register(request):
+	return render_to_response("register.html", { }, context_instance = RequestContext(request) )
+
+#registers a new user, is called from the registering page
+def registering(request):
+	datatable = [request.POST['username'], request.POST['email'], request.POST['password1'], request.POST['password2']]
+	for data in datatable: #checks to see that all fields have data
+		if (len(data) < 1):
+			return render_to_response('register.html', {
+			'error_message': "Error: All fields must be filled.",
+		}, context_instance=RequestContext(request)) #error message for for not filling all fields!
+	if (cmp(request.POST['password1'], request.POST['password2']) != 0 ):
+		return render_to_response('register.html', {
+			'error_message': "Error: Passwords did not match.",
+		}, context_instance=RequestContext(request)) #error for entering two different passwords!
+	user, new = User.objects.get_or_create(username = request.POST['username'])
+	if (new == False):
+		return render_to_response('register.html', {
+			'error_message': "Error: User already exists.",
+		}, context_instance=RequestContext(request)) #error trying the same username two times!
+	else:
+		user.set_password(request.POST['password2'])
+		user.email = request.POST['email']
+		user.is_staff = False
+		user.is_active = False
+		user.save()
+		return render_to_response('login.html', {
+				'error_message': "Account succesfully created.",
+		}, context_instance=RequestContext(request))
+		#new account created - note: it must be activated via the admin panel to be fully functional
+
+#logs a user in, is called from the login page
+def logging_in(request):
+	username = request.POST['username']
+	password = request.POST['password']
+	user = authenticate(username=username, password=password) #djangos auth
+	if user is not None:
+		if user.is_active: #user has to be active to log in
+			login(request, user)
+			return redirect('/graphrefresh')
+		else:
+			return render_to_response('login.html', { #error for a not activated account
+					'error_message': "Account is not active.",
+					}, context_instance = RequestContext(request))
+	else:
+		return render_to_response('login.html', { #login for incorrect username and/or password
+			'error_message': "Username or password was incorrect.",
+		}, context_instance = RequestContext(request))
+
+#logs the user out. Is called from the logout -button
+def logging_out(request):
+	logout(request)
+	return redirect('/')
+
+#saves the users settings
+@login_required
+def savesettings(request):
+		profile = request.user.get_profile()
+		numericsMakeSense = False; #check to see if numeric values make sense. Empty strings are ok, then we just asume they want the defaults
+		duration = request.POST['duration']
+		canvas_x = request.POST['canvas_x']
+		canvas_y = request.POST['canvas_y']
+		if 'defaultsettings' not in request.POST:
+			type = ''
+			transition = ''
+			if request.POST['group1'] == 'animations_off':
+				type = 'replot'
+			else:
+					type = 'fade:con'
+			transition = request.POST['animationtype']
+			subtype = request.POST['animationsubtype']
+			
+			settings = json.dumps({'settings': {'canvaswidth': canvas_x, 'canvasheight': canvas_y}, 'animationsettings': {'type': type, 'duration': duration, 'subtype': subtype, 'transition': transition}})
+			#makes a json out of the settings and saves it
+			profile.settings = settings
+			profile.save()
+			return HttpResponse('Settings saved', mimetype='text/plain')
+		else:
+			profile.settings = '{}'
+			profile.save()
+			return HttpResponse('Restored defaults', mimetype='text/plain')
+
+#saves the graph the user was looking at
+@login_required
+def save_view(request):
+	profile = request.user.get_profile()
+	id_to_delete = request.GET.get('delete')
+	if id_to_delete:
+		try:
+			profile.saved_views.get(pk=id_to_delete).delete()
+		except SavedView.DoesNotExist:
+			pass
+		return HttpResponseRedirect('/graphrefresh')
+	view = profile.saved_views.create(name=request.POST['name'], graph=request.POST['graph'], query=request.POST['query'])
+	return HttpResponse(str(view.id))
+
+@login_required
+def export_view(request):
+	type = request.GET.get('type', 'json')
+	id = int(request.GET['id'])
+
+	view = request.user.get_profile().saved_views.get(pk=id)
+	graph_json = view.graph
+	content = None
+
+	if type == 'json':
+		content = graph_json
+	else:
+		content = 'Invalid "type" argument to export_view'
+
+	return HttpResponse(content, mimetype='text/plain')
+
+
